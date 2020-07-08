b59daf6941c7dcbd8a56faf352ee91de68b5a523
Derek Neilson <42622294+dwerkjem@users.noreply.github.com>
2020-07-08 17:22:45 -0600

    Update Program.cs

--- biaphasprog/biaphasprog/Program.cs
+++ biaphasprog/biaphasprog/Program.cs
@@ -1,50 +1,138 @@
-﻿using System;
-using System.Globalization;
-using System.Threading;
-using System.IO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace bialphasprog
 {
     class Program
-    {
-        static void Main(string[] args)
+	{
+		static void Main(string[] args)
+		{
+			string filename;
 
+			if (args.Length == 0)
+			{
+				Console.Write("file path:");
+				filename = Console.ReadLine();
+			}
+			else
+			{
+				filename = args[0];
+			}
+
+			string[] phraseArray = File.ReadAllLines(filename);
+
+			//ShowPhrases(phrase1);
+
+			List<string> phrases = phraseArray.ToList<string>();
+
+			RemoveSpecialCharacters(ref phrases);
+			ReplaceSpecialCharacters(ref phrases);
+
+			//ShowPhrases(phrases);
+
+			ReplaceNumerics(ref phrases);
+			MakeUpercase(ref phrases);
+			
+			SplitInToBialphas(ref phrases);
+
+
+			ShowPhrases(phrases);
+			Pause(ref phrases);
+		}
+
+
+		private static void ShowPhrases(string[] phrases)
+		{
+			foreach (string phrase in phrases)
+			{
+				Console.WriteLine(phrase);
+			}
+		}
+
+		private static void ShowPhrases(List<string> phrases)
+		{
+			foreach (string phrase in phrases)
+			{
+				Console.WriteLine(phrase);
+			}
+		}
+
+		private static void RemoveSpecialCharacters(ref List<string> phrases)
+		{
+			string[] charactersToRemove = { "@", "_", "-", "(", ")", "+", "=" };
+
+			for (int i = 0; i < phrases.Count; ++i)
+			{
+				foreach (string character in charactersToRemove)
+				{
+					phrases[i] = phrases[i].Replace(character, "");
+				}
+			}
+		}
+
+		private static void ReplaceNumerics(ref List<string> phrases)
+		{
+			for (int i = 0; i < phrases.Count; ++i)
+			{
+				phrases[i] = phrases[i].Replace("0", "ZERO_");
+				phrases[i] = phrases[i].Replace("1", "ONE_");
+				phrases[i] = phrases[i].Replace("2", "TWO_");
+				phrases[i] = phrases[i].Replace("3", "THREE_");
+				phrases[i] = phrases[i].Replace("4", "FOUR_");
+				phrases[i] = phrases[i].Replace("5", "FIVE_");
+				phrases[i] = phrases[i].Replace("6", "SIX_");
+				phrases[i] = phrases[i].Replace("7", "SEVEN_");
+				phrases[i] = phrases[i].Replace("8", "EIGHT_");
+				phrases[i] = phrases[i].Replace("9", "NINE_");
+			}
+
+		}
+		private static void ReplaceSpecialCharacters(ref List<string> phrases)
+		{
+			for (int i = 0; i < phrases.Count; ++i)
+			{
+				phrases[i] = phrases[i].Replace(".", "END_SENTENCE_");
+				phrases[i] = phrases[i].Replace(" ", "_");
+			}
+
+		}
+		private static void MakeUpercase(ref List<string> phrases)
+		{
+            for (int i = 0; i < phrases.Count; ++i)
+			{
+				phrases[i] = phrases[i].ToUpper();
+			}
+		}
+		
+		private static void SplitInToBialphas(ref List<string> phrases)
         {
-            Start:
-            int pair = 0;
-              //|^ Will Be Used later to seprate into bialphas.|
-            Console.Write("file path:");
-            string phrase0 = Console.ReadLine();
-            File.ReadAllLines(phrase0);
-            string[] phrase1 = File.ReadAllLines(phrase0);
-            Console.WriteLine(phrase1);
-            List<string> phrase2 = phrase1.ToList<string>();
-            string[] phrase3 = phrase2.TrimToEnd(v1: '@', v2: '_', v3: '-', v4: ')', v5: '(', v6: '+', v7: '='));
-                                        //| ^Severity	Code	Description	Project	File	Line	Suppression State Error CS0029  Cannot implicitly convert type 'void' to 'object'   |   
-                                        //|biaphasprog C:\Users\Owner\source\repos\bialphasprog\biaphasprog\biaphasprog\Program.cs 23  Active                                           |
-
-            Console.WriteLine(phrase3);
-            string[] phrase4 = phrase3.Replace("0", "ZERO ");
-                                     //|  ^ Severity	Code	Description	Project	File	Line	Suppression  Error CS1061  'List<string>' does not contain a definition for 'Replace' and no accessible |
-                                     //|extension method 'Replace' accepting a first argument of type 'List<string>' could be found(are you missing a using directive or an assembly                        |
-                                     //|reference ?)	biaphasprog C:\Users\Owner\source\repos\bialphasprog\biaphasprog\biaphasprog\Program.cs 27  Active                                                  |
-            phrase4 = phrase4.Replace("1", "ONE ");
-            phrase4 = phrase4.Replace("2", "TWO ");
-            phrase4 = phrase4.Replace("3", "THREE ");
-            phrase4 = phrase4.Replace("4", "FOUR ");
-            phrase4 = phrase4.Replace("5", "FIVE ");
-            phrase4 = phrase4.Replace("6", "SIX ");
-            phrase4 = phrase4.Replace("7", "SEVEN ");
-            phrase4 = phrase4.Replace("8", "EIGHT ");
-            phrase4 = phrase4.Replace("9", "NINE ");
-            phrase4 = phrase4.Replace(".", "END SENTENCE ");
-            phrase4 = phrase4.Replace(" ", "_");
-            Console.WriteLine(phrase4);
-            goto Start;
-        }
-
-        
-    }
+			
+			int pair;
+			for (int i = 0; i < phrases.Count; ++i)
+            {
+				
+				if ((phrases[i].Length % 2) > 0)
+				{
+					//is decimal
+					pair = phrases[i].Length;
+					phrases[i] = phrases+"_";
+					phrases[i] = phrases[i].Insert(pair - 2, " ");
+				}
+				else
+				{
+                    //is int
+                    pair = phrases[i].Length;
+					phrases[i] = phrases[i].Insert(pair - 2, " ");
+                }
+                
+			}
+            
+		}
+		private static void Pause(ref List<string> phrases)
+		{
+			Console.ReadLine();
+		}
+	}
 }
