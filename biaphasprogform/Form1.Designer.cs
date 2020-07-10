08c01074b6178bcd0472d92f0e6de56274fb036d
Derek Neilson <42622294+dwerkjem@users.noreply.github.com>
2020-07-10 10:07:05 -0600

    Add files via upload

---
+++ biaphasprogform/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+
+namespace biaphasprogform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.MaxLength = 9999;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1920, 1080);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1920, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::biaphasprogform.Properties.Resources.Untitled;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 23);
+            this.toolStripButton1.Text = "encript";
+            this.toolStripButton1.ToolTipText = "encript";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1920, 1061);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private readonly EventHandler textBox1_TextChanged;
+    }
+}
+
---
+++ biaphasprogform/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace biaphasprogform
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+		{
+
+		}
+
+		private void toolStripButton1_Click(object sender, EventArgs e)
+		{
+			string text = textBox1.Text;
+
+			{
+				string[] charactersToRemove = { "@", "_", "-", "(", ")", "+", "=", ",", "\"", "'", ";", "`", "\\" };
+
+				foreach (string character in charactersToRemove)
+				{
+					text = text.Replace(character, "");
+				}
+			}
+
+
+			{
+				text = text
+					.Replace("0", "ZERO_")
+					.Replace("1", "ONE_")
+					.Replace("2", "TWO_")
+					.Replace("3", "THREE_")
+					.Replace("4", "FOUR_")
+					.Replace("5", "FIVE_")
+					.Replace("6", "SIX_")
+					.Replace("7", "SEVEN_")
+					.Replace("8", "EIGHT_")
+					.Replace("9", "NINE_");
+			}
+
+
+			{
+				text = text
+					.Replace(".", "END_SENTENCE_")
+					.Replace(" ", "_")
+					.Replace("!", "END_EXCLAMATION_")
+					.Replace("?", "END_Question_");
+			}
+
+
+			{
+				text = text.ToUpper();
+			}
+
+
+			{
+				// make sure the text length is a multiple of 2
+				if ((text.Length % 2) > 0)
+				{
+					text += "_";
+				}
+
+				// store off the length because it is going to change as we add characters
+				int length = text.Length;
+				for (int i = 2; i < length; i += 2)
+				{
+					text = text.Insert(i, " ");
+
+					// skip over the character we just added and increase the length by 1
+					++i;
+					++length;
+				}
+
+				// make sure the text length is a multiple of 2
+				if ((text.Length % 2) > 0)
+				{
+					text += "_";
+				}
+			}
+
+
+			{
+				// Convert Unicode to Bytes
+
+				byte[] uni = Encoding.Unicode.GetBytes(text);
+
+				// Convert to ASCII
+
+				string textAscii = Encoding.ASCII.GetString(uni);
+				// go through each pair of characters and convert them to digits
+				// it should be an even number of characters at this point
+				int length = textAscii.Length;
+				int test = length;
+				for (int i = 0; i < length; i += 5) // + 5 for '0000 ' we want to get to the next place where there is a letter
+				{
+					if (i < length - 2)
+					{
+						for (int ii = 0; ii < 2; ++ii)
+						{
+							int index = i + (ii * 2);
+
+							if (textAscii[index] == '_')
+							{
+								textAscii = textAscii.Insert(index, "00");
+							}
+							else
+							{
+								// - 64 gets 'A' to 1, B to 2, etc.
+								// + 26 gets us to the next bigger number after Z
+
+								int character = (int)textAscii[index];
+								int transformedCharacter = character - 64 + (ii > 0 ? 26 : 0);
+
+								// each letter was one character, each number we are inserting is from 1 to 2 character, let's 0 pad them all to 2 characters
+								// and remove the letter or symbol we just converted
+
+								textAscii = textAscii.Insert(index, transformedCharacter.ToString().PadLeft(2, '0'));
+							}
+
+							textAscii = textAscii.Remove(index + 2, 1);
+						}
+
+						// we have added a 2 characters and then removed 1 so we need to update length
+						length = textAscii.Length;
+						textBox1.Text = text;
+					}
+				}
+			}
+		}
+	}
+}
+
+		
+	
+
