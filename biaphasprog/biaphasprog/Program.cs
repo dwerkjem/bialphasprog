971c80b1162890bd59c4060be25c6c8b70e2c59c
Pete Birch <github@birchfamily.net>
2020-07-09 20:51:05 -0600

    convert character to numbers

--- biaphasprog/biaphasprog/Program.cs
+++ biaphasprog/biaphasprog/Program.cs
@@ -22,7 +22,7 @@ namespace bialphasprog
 
 			string phrases = LoadPhrases(filename);
 
-			//ShowPhrases(phrases);
+			ShowPhrases(phrases);
 
 			RemoveSpecialCharacters(ref phrases);
 			ReplaceSpecialCharacters(ref phrases);
@@ -32,7 +32,7 @@ namespace bialphasprog
 			ReplaceNumerics(ref phrases);
 			MakeUpercase(ref phrases);
 			SplitInToBialphas(ref phrases);
-			firstEncryption(ref phrases);
+			FirstEncryption(ref phrases);
 
 			ShowPhrases(phrases);
 
@@ -59,7 +59,7 @@ namespace bialphasprog
 
 		private static void RemoveSpecialCharacters(ref string phrases)
 		{
-			string[] charactersToRemove = { "@", "_", "-", "(", ")", "+", "=", ",", "\"", "\'"};
+			string[] charactersToRemove = { "@", "_", "-", "(", ")", "+", "=", ",", "\"", "'", ";", "`"};
 
 			foreach (string character in charactersToRemove)
 			{
@@ -87,6 +87,7 @@ namespace bialphasprog
 			phrases = phrases
 				.Replace(".", "END_SENTENCE_")
 				.Replace(" ", "_")
+				.Replace("!", "END_EXCLAMATION_")
 				.Replace("?", "END_Question_");
 		}
 
@@ -127,36 +128,74 @@ namespace bialphasprog
 			Console.ReadKey();
 		}
 
-		private static void firstEncryption(ref string phrases)
+		private static void FirstEncryption(ref string phrases)
         {
-			phrases = phrases
-				.Replace("_", " 0 ")
-				.Replace("A", " 1 ")
-				.Replace("B", " 2 ")
-				.Replace("C", " 3 ")
-				.Replace("D", " 4 ")
-				.Replace("E", " 5 ")
-				.Replace("F", " 6 ")
-				.Replace("G", " 7 ")
-				.Replace("H", " 8 ")
-				.Replace("I", " 9 ")
-				.Replace("J", " 10 ")
-				.Replace("K", " 11 ")
-				.Replace("L", " 12 ")
-				.Replace("N", " 14 ")
-				.Replace("M", " 13 ")
-				.Replace("O", " 15 ")
-				.Replace("P", " 16 ")
-				.Replace("Q", " 17 ")
-				.Replace("R", " 18 ")
-				.Replace("S", " 19 ")
-				.Replace("T", " 20 ")
-				.Replace("U", "21 ")
-				.Replace("V", "22 ")
-				.Replace("W", " 23 ")
-				.Replace("X", " 24 ")
-				.Replace("Y", " 25 ")
-				.Replace("Z", " 26 ");
+			// go through each pair of characters and convert them to digits
+			// it should be an even number of characters at this point
+			int length = phrases.Length;
+			int test = length;
+			for (int i = 0; i < length; i += 5) // + 5 for '0000 ' we want to get to the next place where there is a letter
+			{
+				if (i < length - 2)
+				{
+					for (int ii = 0; ii < 2; ++ii)
+					{
+						int index = i + (ii * 2);
+
+						if (phrases[index] == '_')
+						{
+							phrases = phrases.Insert(index, "00");
+						}
+						else
+						{
+							// - 64 gets 'A' to 1, B to 2, etc.
+							// + 26 gets us to the next bigger number after Z
+
+							int character = (int)phrases[index];
+							int transformedCharacter = character - 64 + (ii > 0 ? 26 : 0);
+
+							// each letter was one character, each number we are inserting is from 1 to 2 character, let's 0 pad them all to 2 characters
+							// and remove the letter or symbol we just converted
+
+							phrases = phrases.Insert(index, transformedCharacter.ToString().PadLeft(2, '0'));
+						}
+
+						phrases = phrases.Remove(index + 2, 1);
+					}
+
+					// we have added a 2 characters and then removed 1 so we need to update length
+					length = phrases.Length;
+				}
+
+			}
+			//phrases = phrases
+			//	.Replace("_", " 0 ")
+			//	.Replace("A", " 1 ")
+			//	.Replace("B", " 2 ")
+			//	.Replace("C", " 3 ")
+			//	.Replace("D", " 4 ")
+			//	.Replace("E", " 5 ")
+			//	.Replace("F", " 6 ")
+			//	.Replace("G", " 7 ")
+			//	.Replace("H", " 8 ")
+			//	.Replace("I", " 9 ")
+			//	.Replace("J", " 10 ")
+			//	.Replace("K", " 11 ")
+			//	.Replace("L", " 12 ")
+			//	.Replace("N", " 14 ")
+			//	.Replace("M", " 13 ")
+			//	.Replace("O", " 15 ")
+			//	.Replace("P", " 16 ")
+			//	.Replace("Q", " 17 ")
+			//	.Replace("R", " 18 ")
+			//	.Replace("S", " 19 ")
+			//	.Replace("T", " 20 ")
+			//	.Replace("U", "21 ")
+			//	.Replace("V", "22 ")
+			//	.Replace("W", " 23 ")
+			//	.Replace("X", " 24 ")
+			//	.Replace("Y", " 25 ")
+			//	.Replace("Z", " 26 ");
 			
 
 		}
