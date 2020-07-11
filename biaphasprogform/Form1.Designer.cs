8f092e33cd0782d96f556f2e794c76b1b144485e
Pete Birch <github@birchfamily.net>
2020-07-10 18:00:59 -0600

    cleaned up code and added a table layout control

--- biaphasprogform/Form1.Designer.cs
+++ biaphasprogform/Form1.Designer.cs
@@ -3,112 +3,121 @@ using System.IO;
 
 namespace biaphasprogform
 {
-    partial class Form1
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(0, 53);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.textBox1.MaxLength = 99999;
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(1920, 920);
-            this.textBox1.TabIndex = 0;
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.progressBar1.Location = new System.Drawing.Point(0, 25);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(1920, 25);
-            this.progressBar1.TabIndex = 2;
-            this.progressBar1.UseWaitCursor = true;
-            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
-            // 
-            // toolStripButton1
-            // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = global::biaphasprogform.Properties.Resources.Untitled;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 2);
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 23);
-            this.toolStripButton1.Text = "encript";
-            this.toolStripButton1.ToolTipText = "encript";
-            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1920, 25);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
-            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1920, 1061);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load_1);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.phraseTextBox = new System.Windows.Forms.TextBox();
+			this.EncryptionProgressBar = new System.Windows.Forms.ProgressBar();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.GoButton = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// phraseTextBox
+			// 
+			this.phraseTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.SetColumnSpan(this.phraseTextBox, 2);
+			this.phraseTextBox.Location = new System.Drawing.Point(0, 31);
+			this.phraseTextBox.Margin = new System.Windows.Forms.Padding(0);
+			this.phraseTextBox.MaxLength = 99999;
+			this.phraseTextBox.Multiline = true;
+			this.phraseTextBox.Name = "phraseTextBox";
+			this.phraseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.phraseTextBox.Size = new System.Drawing.Size(729, 306);
+			this.phraseTextBox.TabIndex = 0;
+			// 
+			// EncryptionProgressBar
+			// 
+			this.EncryptionProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.SetColumnSpan(this.EncryptionProgressBar, 2);
+			this.EncryptionProgressBar.Location = new System.Drawing.Point(3, 3);
+			this.EncryptionProgressBar.Name = "EncryptionProgressBar";
+			this.EncryptionProgressBar.Size = new System.Drawing.Size(723, 25);
+			this.EncryptionProgressBar.TabIndex = 2;
+			this.EncryptionProgressBar.UseWaitCursor = true;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.phraseTextBox, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.EncryptionProgressBar, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.GoButton, 0, 2);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(729, 366);
+			this.tableLayoutPanel1.TabIndex = 3;
+			// 
+			// GoButton
+			// 
+			this.GoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.GoButton.Location = new System.Drawing.Point(3, 340);
+			this.GoButton.Name = "GoButton";
+			this.GoButton.Size = new System.Drawing.Size(75, 23);
+			this.GoButton.TabIndex = 3;
+			this.GoButton.Text = "Encrypt";
+			this.GoButton.UseVisualStyleBackColor = true;
+			this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(729, 366);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Bialpha Encryption";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
 
-        }
+		}
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private readonly EventHandler textBox1_TextChanged;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-    }
+		private System.Windows.Forms.TextBox phraseTextBox;
+		private System.Windows.Forms.ProgressBar EncryptionProgressBar;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button GoButton;
+	}
 }
 
--- biaphasprogform/Form1.cs
+++ biaphasprogform/Form1.cs
@@ -1,182 +1,124 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Media;
-using System.Diagnostics;
 
 namespace biaphasprogform
 {
 	public partial class Form1 : Form
-	
-	{int tick = 0;
+	{
 		public Form1()
 		{
 			InitializeComponent();
-
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-
 		}
 
-		private void Form1_Load_1(object sender, EventArgs e)
+		private void GoButton_Click(object sender, EventArgs e)
 		{
+			//SoundPlayer sound;
+			//sound = new SoundPlayer(@"C:\Users\Owner\source\repos\bialphasprog\bialphasprog\biaphasprogform\obj\Debug\Windows\Background.wav");
+			//sound.Play();
 
-		}
+			string text = phraseTextBox.Text;
 
-		private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-		{
+			string[] charactersToRemove = { "@", "_", "-", "(", ")", "+", "=", ",", "\"", "'", ";", "`", "\\" };
 
-		}
-
-		private void toolStripButton1_Click(object sender, EventArgs e)
-		{
-			string text = textBox1.Text;
-			
+			foreach (string character in charactersToRemove)
 			{
-				string[] charactersToRemove = { "@", "_", "-", "(", ")", "+", "=", ",", "\"", "'", ";", "`", "\\" };
-
-				foreach (string character in charactersToRemove)
-				{
-					text = text.Replace(character, "");
-				}
+				text = text.Replace(character, "");
 			}
 
+			text = text
+				.Replace("0", "ZERO_")
+				.Replace("1", "ONE_")
+				.Replace("2", "TWO_")
+				.Replace("3", "THREE_")
+				.Replace("4", "FOUR_")
+				.Replace("5", "FIVE_")
+				.Replace("6", "SIX_")
+				.Replace("7", "SEVEN_")
+				.Replace("8", "EIGHT_")
+				.Replace("9", "NINE_");
 
+			text = text
+				.Replace(".", "END_SENTENCE_")
+				.Replace(" ", "_")
+				.Replace("!", "END_EXCLAMATION_")
+				.Replace("?", "END_QUESTION_");
+
+			text = text.ToUpper();
+
+			// make sure the text length is a multiple of 2
+			if ((text.Length % 2) > 0)
 			{
-				text = text
-					.Replace("0", "ZERO_")
-					.Replace("1", "ONE_")
-					.Replace("2", "TWO_")
-					.Replace("3", "THREE_")
-					.Replace("4", "FOUR_")
-					.Replace("5", "FIVE_")
-					.Replace("6", "SIX_")
-					.Replace("7", "SEVEN_")
-					.Replace("8", "EIGHT_")
-					.Replace("9", "NINE_");
+				text += "_";
 			}
 
-
+			// store off the length because it is going to change as we add characters
+			int length = text.Length;
+			for (int i = 2; i < length; i += 2)
 			{
-				text = text
-					.Replace(".", "END_SENTENCE_")
-					.Replace(" ", "_")
-					.Replace("!", "END_EXCLAMATION_")
-					.Replace("?", "END_Question_");
+				text = text.Insert(i, " ");
+
+				// skip over the character we just added and increase the length by 1
+				++i;
+				++length;
 			}
 
-
+			// make sure the text length is a multiple of 2
+			if ((text.Length % 2) > 0)
 			{
-				text = text.ToUpper();
+				text += "_";
 			}
 
+			// Convert Unicode to Bytes
 
+			//byte[] uni = Encoding.Unicode.GetBytes(text);
+			// Convert to ASCII
+			//string textAscii = Encoding.ASCII.GetString(uni);
+
+			// go through each pair of characters and convert them to digits
+			// it should be an even number of characters at this point
+			length = text.Length;
+			int test = length;
+			for (int i = 0; i < length; i += 5) // + 5 for '0000 ' we want to get to the next place where there is a letter
 			{
-				// make sure the text length is a multiple of 2
-				if ((text.Length % 2) > 0)
+				for (int ii = 0; ii < 2; ++ii)
 				{
-					text += "_";
-				}
+					int index = i + (ii * 2);
 
-				// store off the length because it is going to change as we add characters
-				int length = text.Length;
-				for (int i = 2; i < length; i += 2)
-				{
-					text = text.Insert(i, " ");
-
-					// skip over the character we just added and increase the length by 1
-					++i;
-					++length;
-				}
-
-				// make sure the text length is a multiple of 2
-				if ((text.Length % 2) > 0)
-				{
-					text += "_";
-				}
-			}
-
-
-			{
-				// Convert Unicode to Bytes
-
-				byte[] uni = Encoding.Unicode.GetBytes(text);
-
-				// Convert to ASCII
-
-				string textAscii = Encoding.ASCII.GetString(uni);
-				// go through each pair of characters and convert them to digits
-				// it should be an even number of characters at this point
-				int length = textAscii.Length;
-				int test = length;
-				for (int i = 0; i < length; i += 5) // + 5 for '0000 ' we want to get to the next place where there is a letter
-				{
-					if (i < length - 2)
+					if (text[index] == '_')
 					{
-						for (int ii = 0; ii < 2; ++ii)
-						{
-							int index = i + (ii * 2);
-
-							if (textAscii[index] == '_')
-							{
-								textAscii = textAscii.Insert(index, "00");
-							}
-							else
-							{
-								// - 64 gets 'A' to 1, B to 2, etc.
-								// + 26 gets us to the next bigger number after Z
-
-								int character = (int)textAscii[index];
-								int transformedCharacter = character - 64 + (ii > 0 ? 26 : 0);
-
-								// each letter was one character, each number we are inserting is from 1 to 2 character, let's 0 pad them all to 2 characters
-								// and remove the letter or symbol we just converted
-
-								textAscii = textAscii.Insert(index, transformedCharacter.ToString().PadLeft(2, '0'));
-							}
-
-							textAscii = textAscii.Remove(index + 2, 1);
-						}
-
-						// we have added a 2 characters and then removed 1 so we need to update length
-						length = textAscii.Length;
-						textBox1.Text = text;
+						text = text.Insert(index, "00");
 					}
+					else
+					{
+						// - 64 gets 'A' to 1, B to 2, etc.
+						// + 26 gets us to the next bigger number after Z
+
+						int character = (int)text[index];
+						int transformedCharacter = character - 64 + (ii > 0 ? 26 : 0);
+
+						// each letter was one character, each number we are inserting is from 1 to 2 character, let's 0 pad them all to 2 characters
+						// and remove the letter or symbol we just converted
+
+						text = text.Insert(index, transformedCharacter.ToString().PadLeft(2, '0'));
+					}
+
+					text = text.Remove(index + 2, 1);
 				}
+
+				// we have added a 2 characters and then removed 1 so we need to update length
+				length = text.Length;
+
+				phraseTextBox.Text = text;
 			}
-           
-		}
-
-        private void progressBar1_Click(object sender, EventArgs e)
-        {
-			SoundPlayer sound;
-			sound = new SoundPlayer(@"C:\Users\Owner\source\repos\bialphasprog\bialphasprog\biaphasprogform\obj\Debug\Windows\Background.wav");
-			sound.Play();
 
 		}
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-			tick = tick + 1;
-        }
-
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-
-        }
-		
 	}
 }
 
-		
-	
+
+
 
